44c8e112fce3f610b0426b931161734ad0949300
Franco Duarte <63573120+francod286@users.noreply.github.com>
2021-07-06 00:38:24 -0300

    Add files via upload

--- js/navegacionJS.js
+++ js/navegacionJS.js
@@ -6,8 +6,7 @@ var texto_nav = `
             </div>
             <ul class="nav-links">
                 <a class="li" href="/index.html">Home</a>                 
-                <a class="li" href="/paginas/about.html">Acerca de</a>      
-                <a class="li" href="/paginas/about.html">Acerca de</a>     
+                <a class="li" href="/paginas/about.html">Acerca de</a>          
                 <a class="li" href="/paginas/contacto.html">Contacto</a>       
                 <a class="li" href="/paginas/sucursal.html">Sucursales</a>    
                 <a class="li" href="/paginas/form.html">Formulario</a>      
